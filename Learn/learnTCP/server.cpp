763aed570c773714a0452addd0727b2cf208a689
lhc <3115629644@qq.com>
2019-04-13 18:50:34 +0800

    commit

--- Learn/learnTCP/server.cpp
+++ Learn/learnTCP/server.cpp
@@ -31,25 +31,35 @@
  
 int main()
 {
-   int listenfd,connfd;
-   socklen_t  clilen;
-   struct sockaddr_in cliaddr,servaddr;
- 
-   listenfd=socket(AF_INET,SOCK_STREAM,0);
-   bzero(&servaddr,sizeof(servaddr));
- 
-   servaddr.sin_family=AF_INET;
-   servaddr.sin_addr.s_addr=INADDR_ANY;
-   servaddr.sin_port=htons(8888);
- 
-   bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));  
-   listen(listenfd,2);
- 
-   getchar();
-   connfd=accept(listenfd,(struct sockaddr *)&cliaddr,&clilen);
- 
-   close(connfd);
-   close(listenfd);
-    return 0;
+    int listenfd,connfd;
+    socklen_t  clilen;
+    struct sockaddr_in cliaddr,servaddr;
+    printf("5\n");
+    listenfd=socket(AF_INET,SOCK_STREAM,0);
+    printf("5\n");
+    if (listenfd < 0) {
+        perror("socket error");
+    }
+    printf("5\n");
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=INADDR_ANY;
+    servaddr.sin_port=htons(8888);
+  
+    printf("5\n");
+    if (bind(listenfd,(struct sockaddr *)&servaddr,sizeof(struct sockaddr)) < 0) {
+        perror("bind error");
+    } else {
+        printf("233444");
+    } 
+    printf("1");
+    listen(listenfd,2);
+  
+    printf("42");
+    connfd=accept(listenfd,(struct sockaddr *)&cliaddr,&clilen);
+     
+    printf("44");
+    close(connfd);
+    close(listenfd);
+     return 0;
 }
 
